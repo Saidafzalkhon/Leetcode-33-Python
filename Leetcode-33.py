3d4fbbe731065e0064c130cfc65da2fa56282017
Saidafzalkhon <Saidafzalkhon.developer@gmail.com>
2022-07-02 14:53:12 +0500

    Add commit

---
+++ Leetcode-33.py
@@ -0,0 +1,9 @@
+nums = list(map(int,input().split()))
+target = int(input())
+for i in range(len(nums)):
+    if nums[i] == target:
+                print(i)
+                break
+                
+else:
+    print(-1)
